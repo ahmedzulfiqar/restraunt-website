d7d79126fef2b71e394d0fbaec22ff547c087c7f
ahmedzulfiqar <ahmdzulfiqar01@example.com>
2023-09-02 09:52:59 +0500

    Updated my React app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,9 @@ import Section4 from "../components/sections/Section4";
 function Home() {
   return (
     <div className="p-0 m-0">
+      <Navbar />
       <Section1 />
-      <Section2 />dsdsdsdsds
+      <Section2 />
       <Section3 />
       <Section4 />
     </div>
