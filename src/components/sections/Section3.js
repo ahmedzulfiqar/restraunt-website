4fd50b496acdb2dde0f2eae753cf89e028b0463f
ahmedzulfiqar <ahmdzulfiqar01@example.com>
2023-09-03 15:44:12 +0500

    ds

--- src/components/sections/Section3.js
+++ src/components/sections/Section3.js
@@ -224,7 +224,7 @@ function Section3() {
       slidesToSlide: 1, // optional, default to 1.
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 1024, min: 0 },
       items: 1,
       slidesToSlide: 1, // optional, default to 1.
     },
